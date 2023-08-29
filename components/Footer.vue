a65e499c53de6e12bf71105045a7556fc50313dd
YunYouJun <me@yunyoujun.cn>
2023-08-30 03:42:08 +0800

    chore: update github link

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,11 @@
+<script lang="ts" setup>
+import pkg from '../package.json'
+</script>
+
 <template>
   <div text="xl gray4" m-5 flex="~ gap3" justify-center>
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+    <a i-carbon-logo-github :href="pkg.repository.url" target="_blank" />
     <DarkToggle />
   </div>
 </template>
